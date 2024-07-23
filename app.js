828fe45b8767de3410025b1075b69b6d289cbac4
Abdulrahman-Khattab <abdulrahman090802302@gmail.com>
2024-07-23 20:51:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const connectDB = require('./db/connect');
+require('express-async-errors');
+require('dotenv');
+
+const app = express();
+
+app.use(express.json());
+
+// static
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  res.send('hello world');
+});
+
+const start = async () => {
+  try {
+    await connectDB(
+      'mongodb+srv://Abdulrahman:12345@nodejsproject.x3hvgne.mongodb.net/?retryWrites=true&w=majority&appName=NodeJSPROJECT'
+    );
+    app.listen(5000, () => {
+      console.log('app listen to port 5000 ');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();

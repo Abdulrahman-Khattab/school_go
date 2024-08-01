d5469362a1456e6bd475332571d9fa540b248140
Abdulrahman-Khattab <abdulrahman090802302@gmail.com>
2024-08-01 17:53:46 +0300

    JSON PARSE ADDED

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -10,6 +10,7 @@ const CONTROLLER_SCHEMA = require('../model/user_controller');
 
 const authenticaiton = async (req, res, next) => {
   let token = req.headers.token;
+  token = JSON.parse(token);
   if (!token) {
     token = req.signedCookies.token;
   }

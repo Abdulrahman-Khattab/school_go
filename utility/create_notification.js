dace98edf9d1169c38947496733fdea283215274
Abdulrahman-Khattab <abdulrahman090802302@gmail.com>
2024-10-20 20:36:16 +0300

    Notification database Record added

--- utility/create_notification.js
+++ utility/create_notification.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-//const serviceAccount = require('../schoolsystem-76875-firebase-adminsdk-1rie4-ca3516ed76.json');
+const Notification = require('../model/notifications');
 
 const sendNotification = async (tokens, payload) => {
   try {
@@ -10,18 +10,33 @@ const sendNotification = async (tokens, payload) => {
 
     // Send the notification using the 'sendMulticast' method for multiple tokens
     const response = await admin.messaging().sendEachForMulticast(message);
-
+    let sucessTokens = [];
     if (response.failureCount > 0) {
       const failedTokens = [];
       response.responses.forEach((resp, idx) => {
         if (!resp.success) {
           failedTokens.push(tokens[idx]);
           console.log('Notification error:', resp.error);
+        } else {
+          sucessTokens.push(tokens[idx]);
         }
       });
       console.log('Failed tokens:', failedTokens);
     }
 
+    if (sucessTokens.length < 1) {
+      sucessTokens = tokens;
+    }
+
+    console.log('Add Notification to data base');
+    await Notification.create({
+      notification: {
+        title: payload.notification.title,
+        body: payload.notification.body,
+      },
+      tokens: sucessTokens,
+      extraData: payload.data.extraData,
+    });
     console.log('Notification sent successfully:', response);
   } catch (error) {
     console.error('Error sending notification:', error.message);
